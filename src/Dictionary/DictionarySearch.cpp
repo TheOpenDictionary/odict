4eae6d6928ac9fffaafb41343333b0113eaf5a94
Tyler Nickerson <nickersoft@gmail.com>
2017-12-16 18:35:45 -0500

    Corrected issue involving search

--- src/Dictionary/DictionarySearch.cpp
+++ src/Dictionary/DictionarySearch.cpp
@@ -1,30 +1,27 @@
 #include "DictionarySearch.h"
 
-DictionarySearch::DictionarySearch(const uint8_t * buffer, const char * format2) : buf(buffer), format(format2) {
-//    if (buffer == 0) {
-//        cout << "Data is corrupted. Cannot read dictionary.\n" << endl;
-//        exit(0);
-//    }
+DictionarySearch::DictionarySearch(const uint8_t *buffer, const char *format2) : buf(buffer), format(format2) {
+    if (buffer == 0) {
+        cout << "Data is corrupted. Cannot read dictionary.\n" << endl;
+        exit(0);
+    }
 }
+DictionarySearch::DictionarySearch(const char *filename) : DictionarySearch(filename, FORMAT_JSON) {}
+DictionarySearch::DictionarySearch(const uint8_t *buffer) : DictionarySearch(buffer, FORMAT_JSON) {}
+DictionarySearch::DictionarySearch(const char *filename, const char *format) : DictionarySearch(
+        (new DictionaryReader())->ReadAsBuffer(filename),
+        format
+) {}
 
-DictionarySearch::DictionarySearch(const char *filename, const char *format) {
-    DictionaryReader *reader = new DictionaryReader();
-    const uint8_t* buf = reader->ReadAsBuffer(filename);
-    DictionarySearch(buf, format);
-}
 
-DictionarySearch::DictionarySearch(const char * filename) {
-    DictionarySearch(filename, FORMAT_JSON);
-}
-
-DictionarySearch::DictionarySearch(const uint8_t * buffer) {
-    DictionarySearch(buffer, FORMAT_JSON);
-}
-
-const char* DictionarySearch::searchByEntry(const char *word) {
-    auto buf = this->buf;
+const char *DictionarySearch::searchByEntry(const char *word) {
     auto entries = GetDictionary(this->buf)->entries();
     auto result = entries->LookupByKey(word);
+    auto converter = ConverterResolver::resolve(this->format);
 
-    return ConverterResolver::resolve(format)->convert(result);
+    if (converter == nullptr) {
+        printf("\nError: could not find resolver for format '%s'\n", this->format);
+        exit(0);
+    }
+    else return converter->convert(result);
 }
