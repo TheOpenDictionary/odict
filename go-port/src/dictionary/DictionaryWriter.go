f0a6f2deb549607eefa33617b67fb68467e12e02
Tyler Nickerson <tnickerson@coursera.org>
2019-12-12 14:48:52 -0800

    cleanup

--- go-port/src/dictionary/DictionaryWriter.go
+++ go-port/src/dictionary/DictionaryWriter.go
@@ -61,74 +61,106 @@ func xmlToDictionary(file *os.File) Dictionary {
 	return dictionary
 }
 
-func dictionaryToBytes(dictionary Dictionary) []byte {
-	builder := flatbuffers.NewBuilder(1024)
+func getUsagesVector(builder *flatbuffers.Builder, ety Etymology) flatbuffers.UOffsetT {
+	var usageBuffer []flatbuffers.UOffsetT
 
-	id := builder.CreateString("id") // TODO: replace
-	name := builder.CreateString(dictionary.Name)
+	for usageIdx, usage := range ety.Usages {
+		usageID := builder.CreateString(strconv.Itoa(usageIdx))
+		usagePOS := builder.CreateString(usage.POS)
 
+		schema.UsageStart(builder)
+		schema.UsageAddId(builder, usageID)
+		schema.UsageAddPos(builder, usagePOS)
+
+		// var groupBuffer []byte
+
+		// for groupIdx, group := range group.Usage {
+		// 	schema.GroupStart(builder)
+		// 	schema.GroupAddId(builder, groupIdx)
+		// 	schema.GroupAddDescription(builder, group.Description)
+		// }
+
+		usageBuffer = append(usageBuffer, schema.UsageEnd(builder))
+	}
+
+	usageCount := len(usageBuffer)
+
+	schema.EtymologyStartUsagesVector(builder, usageCount)
+
+	for i := usageCount - 1; i >= 0; i-- {
+		builder.PrependUOffsetT(usageBuffer[i])
+	}
+
+	return builder.EndVector(usageCount)
+}
+
+func getEtymologiesVector(builder *flatbuffers.Builder, entry Entry) flatbuffers.UOffsetT {
+	var etyBuffer []flatbuffers.UOffsetT
+
+	for etyIdx, ety := range entry.Etymologies {
+		etyID := builder.CreateString(strconv.Itoa(etyIdx))
+		etyDescription := builder.CreateString(ety.Description)
+		etyUsages := getUsagesVector(builder, ety)
+
+		schema.EtymologyStart(builder)
+		schema.EtymologyAddId(builder, etyID)
+		schema.EtymologyAddDescription(builder, etyDescription)
+		schema.EtymologyAddUsages(builder, etyUsages)
+
+		etyBuffer = append(etyBuffer, schema.EtymologyEnd(builder))
+	}
+
+	etyCount := len(etyBuffer)
+
+	schema.EntryStartEtymologiesVector(builder, etyCount)
+
+	for i := etyCount - 1; i >= 0; i-- {
+		builder.PrependUOffsetT(etyBuffer[i])
+	}
+
+	return builder.EndVector(etyCount)
+}
+
+func getEntriesVector(builder *flatbuffers.Builder, dictionary Dictionary) flatbuffers.UOffsetT {
 	var entryBuffer []flatbuffers.UOffsetT
 
 	for entryIdx, entry := range dictionary.Entries {
 		entryID := builder.CreateString(strconv.Itoa(entryIdx)) // TODO: add prefix
 		entryTerm := builder.CreateString(entry.Term)
-
-		var etyBuffer []flatbuffers.UOffsetT
-
-		for etyIdx, ety := range entry.Etymologies {
-			etyID := builder.CreateString(strconv.Itoa(etyIdx))
-			etyDescription := builder.CreateString(ety.Description)
-
-			var usageBuffer []flatbuffers.UOffsetT
-
-			for usageIdx, usage := range ety.Usages {
-				usageID := builder.CreateString(strconv.Itoa(usageIdx))
-				usagePOS := builder.CreateString(usage.POS)
-
-				schema.UsageStart(builder)
-				schema.UsageAddId(builder, usageID)
-				schema.UsageAddPos(builder, usagePOS)
-
-				// var groupBuffer []byte
-
-				// for groupIdx, group := range group.Usage {
-				// 	schema.GroupStart(builder)
-				// 	schema.GroupAddId(builder, groupIdx)
-				// 	schema.GroupAddDescription(builder, group.Description)
-				// }
-
-				usageBuffer = append(usageBuffer, schema.UsageEnd(builder))
-			}
-
-			schema.EtymologyStart(builder)
-			schema.EtymologyAddId(builder, etyID)
-			schema.EtymologyAddDescription(builder, etyDescription)
-			schema.pre
-			schema.EtymologyAddUsages(builder, usageBuffer)
-			etyBuffer = append(etyBuffer, schema.EtymologyEnd(builder))
-		}
+		entryEtymologies := getEtymologiesVector(builder, entry)
 
 		schema.EntryStart(builder)
 		schema.EntryAddId(builder, entryID)
 		schema.EntryAddTerm(builder, entryTerm)
+		schema.EntryAddEtymologies(builder, entryEtymologies)
 
 		entryBuffer = append(entryBuffer, schema.EntryEnd(builder))
 	}
 
+	entryCount := len(entryBuffer)
+
+	schema.DictionaryStartEntriesVector(builder, entryCount)
+
+	for i := entryCount - 1; i >= 0; i-- {
+		builder.PrependUOffsetT(entryBuffer[i])
+	}
+
+	return builder.EndVector(entryCount)
+}
+
+func dictionaryToBytes(dictionary Dictionary) []byte {
+	builder := flatbuffers.NewBuilder(1024)
+
+	id := builder.CreateString("id") // TODO: replace
+	name := builder.CreateString(dictionary.Name)
+	entries := getEntriesVector(builder, dictionary)
+
 	schema.DictionaryStart(builder)
 	schema.DictionaryAddId(builder, id)
 	schema.DictionaryAddName(builder, name)
+	schema.DictionaryAddEntries(builder, entries)
 
-	finishedDictionary := schema.DictionaryEnd(builder)
-
-	// schema.DictionaryStartEntriesVector(builder)
-
-	// schema.DictionaryAddEntries(
-	// 	builder,
-	// 	schema.EntryStart
-	// )
-
-	builder.Finish(finishedDictionary)
+	builder.Finish(schema.DictionaryEnd(builder))
 
 	return builder.FinishedBytes()
 }
