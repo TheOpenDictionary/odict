eaf21b742560295bc1f561f5a4c621b32ad5511e
Tyler Nickerson <tnickerson@coursera.org>
2019-12-12 11:12:01 -0800

    more writer stuff

--- go-port/src/dictionary/DictionaryWriter.go
+++ go-port/src/dictionary/DictionaryWriter.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"odict/schema"
 	"os"
+	"strconv"
 	"time"
 
 	flatbuffers "github.com/google/flatbuffers/go"
@@ -66,16 +67,79 @@ func xmlToDictionary(file *os.File) Dictionary {
 func dictionaryToBuffer(dictionary Dictionary) {
 	builder := flatbuffers.NewBuilder(1024)
 
+	id := builder.CreateString("id") // TODO: replace
+	name := builder.CreateString(dictionary.Name)
+
+	weaponTwo := builder.CreateString("Axe")
+
+	schema.DictionaryStart(builder)
+	schema.DictionaryAddId(builder, id)
+	schema.DictionaryAddName(builder, name)
+
+	var entryBuffer []flatbuffers.UOffsetT
+
+	for entryIdx, entry := range dictionary.Entries {
+		entryId := schema.CreateString(entryIdx) // TODO: add prefix
+		entryTerm := schema.CreateString(entry.Term)
+
+		schema.EntryStart(builder)
+		schema.EntryAddId(builder, entryId)
+		schema.EntryAddTerm(builder, entryTerm)
+
+		var etyBuffer []flatbuffers.UOffsetT
+
+		for etyIdx, ety := range entry.Etymologies {
+			etyID := builder.CreateString(strconv.Itoa(etyIdx))
+			etyDescription := builder.CreateString(ety.Description)
+
+			schema.EtymologyStart(builder)
+			schema.EtymologyAddId(builder, etyID)
+			schema.EtymologyAddDescription(builder, etyDescription)
+
+			var usageBuffer []flatbuffers.UOffsetT
+
+			for usageIdx, usage := range ety.Usages {
+				schema.UsageStart(builder)
+				schema.UsageAddId(builder, usageIdx)
+				schema.UsageAddPos(builder, usage.POS)
+
+				// var groupBuffer []byte
+
+				// for groupIdx, group := range group.Usage {
+				// 	schema.GroupStart(builder)
+				// 	schema.GroupAddId(builder, groupIdx)
+				// 	schema.GroupAddDescription(builder, group.Description)
+				// }
+
+				append(usageBuffer, schema.UsageEnd(builder))
+			}
+
+			append(etyBuffer, schema.EtymologyEnd(builder))
+		}
+
+		append(entryBuffer, schema.EntryEnd(builder))
+	}
+
+	println(entryBuffer)
+
+	// schema.DictionaryStartEntriesVector(builder)
+
+	// schema.DictionaryAddEntries(
+	// 	builder,
+	// 	schema.EntryStart
+	// )
+
+	return builder.Finish()
 }
 
-// GenerateDictionary generates an ODict binary file given
+// WriteDictionary generates an ODict binary file given
 // a ODXML input file path
-func GenerateDictionary(inputPath, outputPath string) {
+func WrtieDictionary(inputPath, outputPath string) {
 	start := time.Now()
 
 	xmlFile := readFile(inputPath)
 	dictionary := xmlToDictionary(xmlFile)
-	schema.DictionaryStart()
+
 	elapsed := time.Since(start)
 
 	fmt.Printf("Completed in %f seconds\n", elapsed.Seconds())
