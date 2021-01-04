7ebb8c7ff820de2577a62594b990b4a9b775ead7
Tyler Nickerson <nickersoft@gmail.com>
2021-01-03 22:21:23 -0800

    started tests

---
+++ go/merge_test.go
@@ -0,0 +1,14 @@
+package odict
+
+import (
+	"testing"
+
+	"github.com/odict/odict/go/models"
+)
+
+func TestMergeDictionaries(t *testing.T) {
+	dict1 := models.Dictionary{
+		ID:   "mockID",
+		Name: "Mock Dictionary 1",
+	}
+}
