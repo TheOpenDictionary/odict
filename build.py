cb503639dd6a7f4c62f07219bad895996dd794e3
Tyler Nickerson <nickersoft@gmail.com>
2022-08-11 15:31:52 -0700

    fixed paths

--- build.py
+++ build.py
@@ -1,13 +1,15 @@
 from distutils.errors import CompileError
+from pathlib import Path
 from subprocess import call
-
+from os import path
 from setuptools import Extension, setup
 from setuptools.command.build_ext import build_ext
 
 
 class build_go_ext(build_ext):
     def build_extension(self, ext):
-        ext_path = self.get_ext_fullpath(ext.name)
+        ext_path = Path(self.get_ext_fullpath(ext.name))
+        ext_path = ext_path.parent.joinpath("odict", ext_path.name).absolute()
         cmd = ["make", "OUTPUT=%s" % ext_path, "lib", "-B"]
         out = call(cmd)
 
