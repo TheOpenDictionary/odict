7defcd6c18bf9beb58f21d6b4eba2e1190440bd9
Tyler Nickerson <nickersoft@gmail.com>
2023-10-25 09:35:05 -0700

    wip

--- xsd/xsd.go
+++ xsd/xsd.go
@@ -8,39 +8,59 @@ import (
 	"github.com/TheOpenDictionary/odict/lib/types"
 )
 
-func generateXSD(indent int, t reflect.Type) string {
-	xsd := fmt.Sprintf("%s<xs:complexType>\n", strings.Repeat(" ", indent))
+func generateXSD(tagName string, attribute bool, t reflect.Type, indent int) string {
+	name := t.Name()
 
-	for i := 0; i < t.NumField(); i++ {
-		field := t.Field(i)
-		fieldType := field.Type
-		fieldName := fieldType.Name()
-		tag := field.Tag.Get("xml")
-		tagName := strings.Split(tag, ",")[0]
-
-		if fieldName == "Name" {
-			continue
-		}
-
-		if fieldType.Kind() == reflect.Map {
-			xsd += generateXSD(indent+2, fieldType.Elem())
-		} else if fieldType.Kind() == reflect.Slice {
-			xsd += fmt.Sprintf("%s<xs:sequence>\n", strings.Repeat(" ", indent))
-			println(fieldType.Elem().Name())
-			xsd += generateXSD(indent+4, fieldType.Elem())
-			xsd += fmt.Sprintf("%s</xs:sequence>\n", strings.Repeat(" ", indent))
-		} else {
-			xsd += fmt.Sprintf("%s<xs:element name=\"%s\" type=\"xs:%s\" />\n", strings.Repeat(" ", indent+2), tagName, fieldName)
-		}
+	if name == "Name" {
+		return ""
 	}
 
-	xsd += fmt.Sprintf("%s</xs:complexType>\n", strings.Repeat(" ", indent))
+	if name == "PartOfSpeech" || name == "MDString" {
+		return generateXSD(tagName, attribute, reflect.TypeOf(""), indent)
+	}
 
-	return xsd
+	if t.Kind() == reflect.Map {
+		return generateXSD(tagName, attribute, t.Elem(), indent+2)
+	}
+
+	if t.Kind() == reflect.Slice {
+		xsd := fmt.Sprintf("%s<xs:sequence>\n", strings.Repeat(" ", indent))
+		xsd += generateXSD(tagName, attribute, t.Elem(), indent+2)
+		xsd += fmt.Sprintf("%s</xs:sequence>\n", strings.Repeat(" ", indent))
+		return xsd
+	}
+
+	if t.Kind() == reflect.Struct {
+		xsd := fmt.Sprintf("%s<xs:element name=\"%s\">\n", strings.Repeat(" ", indent), tagName)
+		xsd += fmt.Sprintf("%s<xs:complexType>\n", strings.Repeat(" ", indent+2))
+
+		for i := 0; i < t.NumField(); i++ {
+			field := t.Field(i)
+			fieldType := field.Type
+			tag := field.Tag.Get("xml")
+			tagName := strings.Split(tag, ",")[0]
+			attr := strings.Contains(tag, "attr")
+
+			xsd += generateXSD(tagName, attr, fieldType, indent+4)
+		}
+
+		xsd += fmt.Sprintf("%s</xs:complexType>\n", strings.Repeat(" ", indent+2))
+		xsd += fmt.Sprintf("%s</xs:element>\n", strings.Repeat(" ", indent))
+
+		return xsd
+	}
+
+	tag := "element"
+
+	if attribute {
+		tag = "attribute"
+	}
+
+	return fmt.Sprintf("%s<xs:%s name=\"%s\" type=\"xs:%s\" />\n", strings.Repeat(" ", indent), tag, tagName, name)
 }
 
 func main() {
-	xsd := generateXSD(0, reflect.TypeOf(types.DictionaryRepresentable{}))
+	xsd := generateXSD("dictionary", false, reflect.TypeOf(types.DictionaryRepresentable{}), 0)
 
 	print(xsd)
 }
