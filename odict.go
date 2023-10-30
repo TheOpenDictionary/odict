82822c6fcae2757dd28f4013ea65c02641d608cf
Tyler Nickerson <nickersoft@gmail.com>
2023-10-29 17:54:20 -0700

    wip

--- odict.go
+++ odict.go
@@ -5,14 +5,9 @@ import (
 	"os"
 
 	"github.com/TheOpenDictionary/odict/cli"
-	"github.com/TheOpenDictionary/odict/lib/validator"
 )
 
 func main() {
-	validator.InitializeValidator()
-
-	defer validator.CleanupValidator()
-
 	err := cli.App.Run(os.Args)
 
 	if err != nil {
