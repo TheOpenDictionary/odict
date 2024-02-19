a4921ed54614fb4897f8663124606816c9a7bac3
Tyler Nickerson <nickersoft@gmail.com>
2024-02-18 17:58:17 -0800

    idk

--- lib/src/search/index.rs
+++ lib/src/search/index.rs
@@ -15,7 +15,7 @@ impl Dictionary {
         let mut schema_builder = Schema::builder();
         let index = Index::create_in_dir(&index_path, schema.clone())?;
         schema_builder.add_text_field("title", TEXT | STORED);
-
+        // schema_builder.
         let schema = schema_builder.build();
     }
 }
