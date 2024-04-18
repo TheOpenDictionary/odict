ed14863866c2f29e79acce5a3361b06fac27c7a6
Tyler Nickerson <nickersoft@gmail.com>
2024-04-18 01:49:44 -0700

    wip

--- node/src/lib.rs
+++ node/src/lib.rs
@@ -1,9 +1,44 @@
 #![deny(clippy::all)]
 
+use odict::{DictionaryFile, DictionaryReader};
+
 #[macro_use]
 extern crate napi_derive;
 
+#[napi(object)]
+pub struct DictionaryOptions {
+  pub default_split_threshold: Option<u32>,
+}
+
+impl Default for DictionaryOptions {
+  fn default() -> Self {
+    DictionaryOptions {
+      default_split_threshold: None,
+    }
+  }
+}
+
 #[napi]
-pub fn sum(a: i32, b: i32) -> i32 {
-  a + b
+struct Dictionary {
+  pub path: String,
+  options: Option<DictionaryOptions>,
+  file: DictionaryFile,
+}
+
+#[napi]
+impl Dictionary {
+  #[napi(constructor)]
+  pub fn new(path: String, options: Option<DictionaryOptions>) -> napi::Result<Self> {
+    let reader = DictionaryReader::default();
+
+    let file = reader
+      .read_from_path(&path)
+      .map_err(|e| napi::Error::new(napi::Status::GenericFailure, e))?;
+
+    Ok(Dictionary {
+      path,
+      options,
+      file,
+    })
+  }
 }
