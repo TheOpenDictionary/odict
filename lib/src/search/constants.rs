81531528f8e69f19b258d000e4fa412bed81c9ac
Tyler Nickerson <nickersoft@gmail.com>
2025-03-30 01:25:57 -0400

    wip

--- lib/src/search/constants.rs
+++ lib/src/search/constants.rs
@@ -2,21 +2,10 @@ use std::sync::LazyLock;
 
 use tantivy::tokenizer::TextAnalyzer;
 
-#[cfg(feature = "charabia")]
 use super::charabia::CharabiaTokenizer;
-
-#[cfg(not(feature = "charabia"))]
-use tantivy::tokenizer::{LowerCaser, RemoveLongFilter, SimpleTokenizer};
 
 pub const CUSTOM_TOKENIZER: &str = "CUSTOM_TOKENIZER";
 
 pub const DEFAULT_TOKENIZER: LazyLock<TextAnalyzer> = LazyLock::new(|| {
-    #[cfg(not(feature = "charabia"))]
-    return TextAnalyzer::builder(SimpleTokenizer::default())
-        .filter(RemoveLongFilter::limit(40))
-        .filter(LowerCaser)
-        .build();
-
-    #[cfg(feature = "charabia")]
     return CharabiaTokenizer::default().into();
 });
