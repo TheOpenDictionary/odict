2378d3c4848112a37da19924c3df9c22202d70d1
Tyler Nickerson <nickersoft@gmail.com>
2026-06-19 23:53:35 -0400

    gen cli

--- crates/docgen/src/main.rs
+++ crates/docgen/src/main.rs
@@ -1,11 +1,11 @@
 use std::path::Path;
 
-mod cli_reference;
+mod cli;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let workspace_root = Path::new(env!("CARGO_MANIFEST_DIR")).join("../..");
 
-    cli_reference::generate_docs(workspace_root.join("docs/src/content/docs/cli/reference.md"))?;
+    cli::generate_docs(workspace_root.join("docs/src/content/docs/cli/reference.md"))?;
 
     Ok(())
 }
